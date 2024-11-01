2546c4f1c3107570d4f70a4d1ea6eefbab699ead
Makenley Luctamar <luctamak001@warren.k12.in.us>
2024-11-01 07:15:33 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#TODO: Enter your age as an int.  Using the input function.
+age = 17
+
+if age < 5:
+    print("You should play with toys.")
+
+elif age >= 5 and age <= 12:
+    print("You can play outdoor game.")
+
+elif age >= 13 and age <= 17:
+    print("You might enjoy video games.")
+
+elif age < 18:
+    print("You can try hiking or traveling.")
